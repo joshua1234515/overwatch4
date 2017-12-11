a770b86a3f690c0f47e5d32e946074005c8b93ae
joshua1234515 <joshua_jimenez@davincischools.org>
2017-12-11 15:53:15 -0800

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,28 @@
+function showHog(elem){
+	elem.classList.toggle("hog2");
+	elem.classList.toggle("hog");
+}
+function showDiva(elem){
+	elem.classList.toggle("diva1");
+	elem.classList.toggle("diva2");
+}
+function showWinston(elem){
+	elem.classList.toggle("Winston1");
+	elem.classList.toggle("W1");
+}
+function showReinhardt(elem){
+	elem.classList.toggle("r1");
+	elem.classList.toggle("r2");
+}
+function showZarya(elem){
+	elem.classList.toggle("Z2");
+	elem.classList.toggle("Z1");
+}
+function showOsiris(elem){
+	elem.classList.toggle("O2");
+	elem.classList.toggle("O1");
+}
+function showTank(elem){
+	elem.classList.toggle("tanks");
+	elem.classList.toggle("tanks");
+}
